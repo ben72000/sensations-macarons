588b4b20bfbf99dc285121c64770fe0b9c3409bc
ben72000 <132427668+ben72000@users.noreply.github.com>
2026-06-28 07:24:00 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE = 'sm-iphone-v1006';
+const CACHE = 'sm-iphone-v1009';
 const ASSETS = [
   './',
   './index.html',
-  './utils.js?v=1006',
-  './app.js?v=1006',
+  './utils.js?v=1009',
+  './app.js?v=1009',
   './dexie.min.js',
-  './qr_min.js?v=1006',
+  './qr_min.js?v=1009',
   './pdf_extract.js',
   './manifest.webmanifest',
   './icon-192.png',
@@ -25,7 +25,7 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c =>
       c.addAll(ASSETS).then(()=>{
-        return c.add('./html5-qrcode.min.js?v=1006').catch(()=>{});
+        return c.add('./html5-qrcode.min.js?v=1009').catch(()=>{});
       })
     ).then(() => self.skipWaiting())
   );
